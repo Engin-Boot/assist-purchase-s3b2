4c85a4f81d11ae21fea9975f3a4f5fea0fedf27a
spooja-chopra <69622617+spooja-chopra@users.noreply.github.com>
2020-10-21 07:30:24 +0530

    Update ProductsRepository.java

--- src/main/java/com/philips/casestudy/product/ProductsRepository.java
+++ src/main/java/com/philips/casestudy/product/ProductsRepository.java
@@ -9,4 +9,9 @@ public interface ProductsRepository extends JpaRepository<Products,Integer>
 {
 	@Query(value="SELECT * FROM products WHERE category=?1 OR name=?2 ",nativeQuery=true)
 	List<Products> findByCategoryOrName(String category,String name);
+	
+	@Query(value="SELECT * FROM products WHERE category=?1 OR name=?2 ",nativeQuery=true)
+	List<Products> f(String category,String name);
+	@Query(value="SELECT * FROM products WHERE category=?1 OR name=?2 ",nativeQuery=true)
+	List<Products> findByCategoryOrName(String category,String name);
 }
